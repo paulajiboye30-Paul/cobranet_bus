c42f4173345dc01934dce5ab888706883d9f1393
paulajiboye30-Paul <paulajiboye30@gmail.com>
2026-04-25 12:22:03 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use('/api', async (req, res, next) => {
 });
 
 app.use('/api', require('./routes/staffRoutes'));
+app.use('/api', require('./routes/busRouteRoutes'));
 app.use('/api', require('./routes/routesreservationRoutes'));
 app.use('/api', require('./routes/routessettingsRoutes'));
 app.use('/api/cron', require('./routes/cronRoutes'));
